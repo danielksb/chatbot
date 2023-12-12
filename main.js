82e9d4aa38774b2fe3eabda26735946dc2060e27
Daniel Käs <daniel.kaes@atos.net>
2023-12-12 20:59:55 +0100

    feat: translates speech to text

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const multer = require('multer');
+const openai = require('openai');
+const {toFile} = require('openai/uploads')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -22,12 +24,12 @@ const upload = multer({ storage: storage });
 // Endpoint to receive and store audio data
 app.post('/upload', upload.single('audio'), (req, res) => {
     const audioData = req.file.buffer;
-    
+
     if (!audioData) {
         return res.status(400).json({ error: 'No audio data provided' });
     }
 
-    const fileName = `audio_${Date.now()}.ogg`;
+    const fileName = `audio_${Date.now()}.wav`;
     const filePath = path.join(tempDir, fileName);
 
     fs.writeFile(filePath, audioData, 'binary', (err) => {
@@ -41,7 +43,47 @@ app.post('/upload', upload.single('audio'), (req, res) => {
     });
 });
 
+// Endpoint to convert audio data to text
+app.post('/speech2text', upload.single('audio'), async (req, res) => {
+    const audioData = req.file.buffer;
+
+    if (!audioData) {
+        return res.status(400).json({ error: 'No audio data provided' });
+    }
+
+    try {
+        const spokenText = await convertSpeechToText(audioData);
+        res.status(200).json({ text: spokenText });
+    } catch (error) {
+        console.error("ERROR", error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+/**
+ * Converts recorded audio data to spoken text
+ * @param {Buffer} audioData recorded audio data
+ * @returns spoken text
+ */
+async function convertSpeechToText(audioData) {
+    const apiKey = process.env.OPENAI_API_KEY;
+    if (!apiKey) {
+        throw new Error('OpenAI API key not found. Set the OPENAI_API_KEY environment variable.');
+    }
+
+    const file = await toFile(audioData, "record.wav", {type: 'audio/wav'});
+
+    const client = new openai.OpenAI();
+    client.apiKey = apiKey;
+    const transcription = await client.audio.transcriptions.create({
+        file: file,
+        model: "whisper-1",
+        });
+        console.log("INFO", transcription)
+    return transcription.text;
+}
--- static/script.js
+++ static/script.js
@@ -1,17 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     let recorder;
-    let audioChunks = [];
-    let recordedAudioUrl;
 
     const recordButton = document.getElementById('recordButton');
-    const playButton = document.getElementById('playButton');
-    let audioElement;
+    const resultContainer = document.getElementById('resultContainer');
+    const spokenTextElement = document.getElementById('spokenText');
 
     recordButton.addEventListener('click', () => {
         if (!recorder) {
             navigator.mediaDevices.getUserMedia({ audio: true })
                 .then(stream => {
                     recorder = new MediaRecorder(stream);
+                    const audioChunks = [];
 
                     recorder.ondataavailable = event => {
                         if (event.data.size > 0) {
@@ -20,16 +19,21 @@ document.addEventListener('DOMContentLoaded', function () {
                     };
 
                     recorder.onstop = () => {
-                        const audioBlob = new Blob(audioChunks, { type: 'audio/ogg; codecs=opus' });
-                        recordedAudioUrl = URL.createObjectURL(audioBlob);
-                        playButton.disabled = false;
+                        const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+
+                        // Send the audio data to the backend
+                        sendAudioData(audioBlob)
+                            .then(response => {
+                                const spokenText = response.text;
+                                resultContainer.style.display = 'block';
+                                spokenTextElement.textContent = spokenText;
+                            })
+                            .catch(error => {
+                                console.error('Error sending audio data:', error);
+                            });
 
                         // Reset recorder and audioChunks
                         recorder = null;
-                        audioChunks = [];
-
-                        // Send the audio data to the backend
-                        sendAudioData(audioBlob);
                     };
 
                     recorder.start();
@@ -44,29 +48,12 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    playButton.addEventListener('click', () => {
-        if (recordedAudioUrl) {
-            // Remove existing audio element if it exists
-            if (audioElement?.parentNode) {
-                audioElement.parentNode.removeChild(audioElement);
-            }
-            // Create new audio element
-            const audio = new Audio(recordedAudioUrl);
-            audio.controls = true;
-            document.body.appendChild(audio);
-            audio.play();
-        }
-    });
-
-    /**
-     * Function to send audio data to the backend
-     * @param {Blob} audioBlob
-     */
+    // Function to send audio data to the backend
     function sendAudioData(audioBlob) {
         const formData = new FormData();
-        formData.append('audio', audioBlob, 'recorded_audio.ogg');
+        formData.append('audio', audioBlob, 'recorded_audio.wav');
 
-        fetch('/upload', {
+        return fetch('/speech2text', {
             method: 'POST',
             body: formData,
         })
@@ -75,13 +62,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
             return response.json();
-        })
-        .then(data => {
-            // Handle the response from the server if needed
-            console.log('Audio data sent successfully:', data);
-        })
-        .catch(error => {
-            console.error('Error sending audio data to the server:', error);
         });
     }
 });
